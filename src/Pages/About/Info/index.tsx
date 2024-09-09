ac4702439b66eaf6e83bed88bca5f9114330a477
Kelly Guimarans <kelly.guimarans@gmail.com>
2024-09-08 23:38:50 -0300

    feat: finalizacao do layout

--- src/Pages/About/Info/index.tsx
+++ src/Pages/About/Info/index.tsx
@@ -22,46 +22,6 @@ export default function Info() {
         name="Spider-man"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eleifend metus in tincidunt blandit. Donec sollicitudin maximus accumsan. Sed condimentum ipsum eu lacus suscipit luctus. Nam eleifend orci at diam pharetra tincidunt. Praesent eu metus viverra."
       />
-
-      <Thumbnail
-        format="horizontal"
-        image="https://placehold.co/200x212"
-        id="3"
-        date="01/01/2015"
-        nPage="47 pages"
-        name="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium metus interdum dolor."
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eleifend metus in tincidunt blandit. Donec sollicitudin maximus accumsan. Sed condimentum ipsum eu lacus suscipit luctus. Nam eleifend orci at diam pharetra tincidunt. Praesent eu metus viverra."
-      />
-
-      <Thumbnail
-        format="horizontal"
-        image="https://placehold.co/200x212"
-        id="4"
-        date="01/01/2015"
-        nPage="47 pages"
-        name="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium metus interdum dolor."
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eleifend metus in tincidunt blandit. Donec sollicitudin maximus accumsan. Sed condimentum ipsum eu lacus suscipit luctus. Nam eleifend orci at diam pharetra tincidunt. Praesent eu metus viverra."
-      />
-
-      <Thumbnail
-        format="horizontal"
-        image="https://placehold.co/200x212"
-        id="5"
-        date="01/01/2015"
-        nPage="47 pages"
-        name="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium metus interdum dolor."
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eleifend metus in tincidunt blandit. Donec sollicitudin maximus accumsan. Sed condimentum ipsum eu lacus suscipit luctus. Nam eleifend orci at diam pharetra tincidunt. Praesent eu metus viverra."
-      />
-
-      <Thumbnail
-        format="horizontal"
-        image="https://placehold.co/200x212"
-        id="6"
-        date="01/01/2015"
-        nPage="47 pages"
-        name="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium metus interdum dolor."
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eleifend metus in tincidunt blandit. Donec sollicitudin maximus accumsan. Sed condimentum ipsum eu lacus suscipit luctus. Nam eleifend orci at diam pharetra tincidunt. Praesent eu metus viverra."
-      />
     </section>
   );
 }
